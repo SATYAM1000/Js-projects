e06d7439e1fe8b7275bb7a88b91ffc3ad3abb271
satyam <satyamsingh748846@gmail.com>
2025-02-02 10:44:39 +0530

    feat: initalized snake-game

---
+++ snake-game/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+            return "styles.css";
+          }
+          return "[name].[ext]";
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
